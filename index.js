3aca727a2254dcc76b02e23cf7442d491cac1d1b
Bit9-A <92058084+Bit9-A@users.noreply.github.com>
2025-01-07 18:08:50 -0400

    Agregar soporte para CORS

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-import 'dotenv/config'
-import express from "express"
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 import UserRouter from './src/routes/users.route.js';
 import clientRoutes from './src/routes/clients.route.js';
-import employeeRoutes from './src/routes/employee.route.js'
-import proposalRoutes from './src/routes/proposal.route.js'
+import employeeRoutes from './src/routes/employee.route.js';
+import proposalRoutes from './src/routes/proposal.route.js';
 import serviceRoutes from './src/routes/services.route.js';
+import projectsRouter from './src/routes/projects.route.js';
+import notifications from './src/routes/notifications.route.js';
+
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use('/users', UserRouter)
+// Configurar CORS
+app.use(cors({
+  origin: 'http://localhost:8000', // Reemplaza con la URL de tu frontend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', UserRouter);
 app.use('/clients', clientRoutes);
 app.use('/employees', employeeRoutes);
 app.use('/proposal', proposalRoutes);
 app.use('/services', serviceRoutes);
-
-
-
+app.use('/projects', projectsRouter);
+app.use('/notifications', notifications);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log('Servidor en '+ PORT));
+app.listen(PORT, () => console.log('Servidor en ' + PORT));
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -7,7 +7,6 @@ const router = express.Router();
 // Rutas para users
 router.get('/', verifyToken, UserController.getAllUsers);
 router.get('/:id', verifyToken, UserController.getUserById);
-router.post('/', verifyToken, UserController.createUser);
 router.put('/:id', verifyToken, UserController.updateUser);
 router.delete('/:id', verifyToken, UserController.deleteUser);
 
@@ -17,5 +16,11 @@ router.get('/roles/:id', verifyToken, UserController.getUserRoleById);
 router.post('/roles', verifyToken, UserController.createUserRole);
 router.put('/roles/:id', verifyToken, UserController.updateUserRole);
 router.delete('/roles/:id', verifyToken, UserController.deleteUserRole);
+
+// Rutas para registro, login, perfil y logout
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+router.get('/profile', verifyToken, UserController.profile);
+router.post('/logout', verifyToken, UserController.logout);
 
 export default router;
